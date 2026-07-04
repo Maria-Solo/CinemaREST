51bc5c857085587a9cb09ecdfc13f3ecb8a6e2fe
Маша <mariya.lebedeva.9625@gmail.com>
2026-07-04 20:51:26 +0700

    Add session controller + DTOs

--- CinemaREST/src/main/java/com/example/mary/CinemaREST/dto/SessionRequestDTO.java
+++ CinemaREST/src/main/java/com/example/mary/CinemaREST/dto/SessionRequestDTO.java
@@ -1,4 +1,23 @@
 package com.example.mary.CinemaREST.dto;
 
+import com.example.mary.CinemaREST.entity.Movie;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class SessionRequestDTO {
+    //private Long movieId;
+    /// добавить валидацию на то, что фильм должен существовать
+    private Movie movie;
+    private LocalDateTime startTime;
+
+    @Min(value = 1, message = "Номер зала должен быть больше 0")
+    private int hallNumber;
+
+    @Min(value = 1, message = "Количество мест должно быть больше 0")
+    @Max(value = 300, message = "Количество мест не может превышать 300")
+    private int totalSeats;
 }
--- CinemaREST/src/main/java/com/example/mary/CinemaREST/dto/SessionResponseDTO.java
+++ CinemaREST/src/main/java/com/example/mary/CinemaREST/dto/SessionResponseDTO.java
@@ -1,4 +1,13 @@
 package com.example.mary.CinemaREST.dto;
 
+import java.time.LocalDateTime;
+import lombok.Data;
+
+@Data
 public class SessionResponseDTO {
+    private Long id;
+    private Long movieId;
+    public LocalDateTime startTime;
+    public int hallNumber;
+    public int totalSeats;
 }
