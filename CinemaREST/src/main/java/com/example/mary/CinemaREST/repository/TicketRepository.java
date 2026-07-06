32dd1d760ea77c6b8ebbad829d21debc6d32a46a
Маша <mariya.lebedeva.9625@gmail.com>
2026-07-06 22:41:54 +0700

    Add TicketService and two ticket DTOs (just an outline)

--- CinemaREST/src/main/java/com/example/mary/CinemaREST/repository/TicketRepository.java
+++ CinemaREST/src/main/java/com/example/mary/CinemaREST/repository/TicketRepository.java
@@ -4,7 +4,12 @@ import com.example.mary.CinemaREST.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     boolean existsBySessionIdAndSeatNumber(Long sessionId, int seatNumber);
+
+    //what's this?
+    List<Ticket> id(Long id);
 }
