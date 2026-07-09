975ff4e81586ae2d3c4ce26067499e3b5ebeca21
mariy <workmail@gmail.com>
2026-07-09 18:28:00 +0700

    Add ticket DTOs and started controller methods

--- CinemaREST/src/main/java/com/example/mary/CinemaREST/dto/TicketRequestDTO.java
+++ CinemaREST/src/main/java/com/example/mary/CinemaREST/dto/TicketRequestDTO.java
@@ -1,4 +1,10 @@
 package com.example.mary.CinemaREST.dto;
 
+import lombok.Data;
+
+@Data
 public class TicketRequestDTO {
+    private Long sessionId;
+    private String customerName;
+    private Integer seatNumber;
 }
--- CinemaREST/src/main/java/com/example/mary/CinemaREST/dto/TicketResponseDTO.java
+++ CinemaREST/src/main/java/com/example/mary/CinemaREST/dto/TicketResponseDTO.java
@@ -1,4 +1,11 @@
 package com.example.mary.CinemaREST.dto;
 
+import lombok.Data;
+
+@Data
 public class TicketResponseDTO {
+    private Long id;
+    private Long sessionId;
+    private String customerName;
+    private Integer seatNumber;
 }
